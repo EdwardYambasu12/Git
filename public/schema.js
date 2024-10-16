e39234bd38b14549e6cedf82627ccc1d97e54b06
EdwardYambasu12 <yambasuedward4@gmial.com>
2024-10-16 22:13:08 +0000

    google main

---
+++ public/schema.js
@@ -0,0 +1,15 @@
+{
+  "@context": "https://schema.org",
+  "@type": "Website",
+  "name": "SportsUp",
+  "url": "https://www.sportsupd.com",
+  "description": "Your ultimate destination for sports news, events, and community discussions. SportsUp is the essential app for matchday. Get live scores, fixtures, tables, match stats, and personalised news from over 500 football leagues around the world. SportsUp ",
+  "sameAs": [
+   
+  ],
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://www.sportsup.com/search?q={search_term_string}",
+    "query-input": "required name=search_term_string"
+  }
+}
