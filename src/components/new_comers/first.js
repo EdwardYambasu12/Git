d116379fee82b8495610e20c2216f2592749a888
EdwardYambasu12 <yambasuedward4@gmial.com>
2024-12-24 12:37:31 +0000

    Most of us£

--- src/components/new_comers/first.js
+++ src/components/new_comers/first.js
@@ -53,25 +53,15 @@ console.log(automain, "this is the counterpart")
 
           {/* Content */}
           <div style={{ position: "relative", zIndex: 10 }}>
-            <h1 style={{ fontSize: "2.5rem", marginBottom: "10px" }}>
+            <h3 style={{  marginBottom: "10px" }}>
             Welcome to SportsUp: Your Ultimate Sports Companion
-            </h1>
-            <p style={{ fontSize: "1.2rem", maxWidth: "600px", margin: "0 auto" }}>
+            </h3>
+            <p style={{ fontSize: "1rem", maxWidth: "600px", margin: "0 auto" }}>
             SportsUp is a comprehensive platform designed for sports enthusiasts who want to stay informed, connected, and engaged with their favorite sports, teams, leagues, and players. Whether you're a casual fan or a dedicated follower, SportsUp provides all the tools and content you need to elevate your sports experience. Here’s a deep dive into what SportsUp offers and how it can transform the way you interact with the sports world.
             </p>
 
             {/* Featured Image */}
-            <img
-              src="https://source.unsplash.com/random/600x400?football"
-              alt="Football Highlights"
-              style={{
-                width: "100%",
-                maxWidth: "400px",
-                borderRadius: "10px",
-                margin: "20px auto",
-                boxShadow: "0 4px 8px rgba(0,0,0,0.5)",
-              }}
-            />
+           
 
             <button
               onClick={()=>{navigate("/follow_up")}}
