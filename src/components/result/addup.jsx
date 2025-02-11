71f27bc79fb60fb8e8243c93819145b4d434ea3d
EdwardYambasu12 <yambasuedward4@gmial.com>
2025-02-11 16:57:49 +0000

    God innit

--- src/components/result/addup.jsx
+++ src/components/result/addup.jsx
@@ -16,7 +16,7 @@ const Bracket = ({data}) => {
             <div>
               
 
-                {data.draw.configuration.map((item)=>{
+                {data.draw != null ? data.draw.configuration.map((item)=>{
                     return(
                             <div style = {{background : "white", borderRadius : "10px"}} className = "container">
                                     <h5 className = "text-center">{item.stage}</h5>
@@ -50,7 +50,68 @@ const Bracket = ({data}) => {
                                     })}
                             </div>
                         )
+                }) : <><div>
+    {data.rounds.map((round, roundIndex) => {
+        return (
+            <div
+                key={roundIndex}
+                style={{
+                    background: "white",
+                    borderRadius: "10px",
+                    display: "flex",
+                    flexFlow: 1,
+                    flex: 0,
+                    width: "100%",
+                    flexWrap : "wrap",
+                    justifyContent : "space-between"
+                }}
+                className="container"
+            >
+              
+                {round.matchups.map((matchup, matchupIndex) => {
+                    return (
+                        <div
+                            key={matchupIndex}
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                width: "22%",
+                                background: "lightgray",
+                                borderRadius: "7px",
+                            }}
+                        >
+                            <div>
+                                <img
+                                    src={`https://images.fotmob.com/image_resources/logo/teamlogo/${matchup.homeTeamId}_xsmall.png`}
+                                    style={{ width: "20px", height: "20px" }}
+                                    alt={`${matchup.homeTeamShortName} logo`}
+                                />
+                                <h6>{matchup.homeTeamShortName}</h6>
+                                <h6>
+                                    <strong>{matchup.aggregatedResult.homeScore}</strong>
+                                </h6>
+                            </div>
+
+                            <div>
+                                <img
+                                    src={`https://images.fotmob.com/image_resources/logo/teamlogo/${matchup.awayTeamId}_xsmall.png`}
+                                    style={{ width: "20px", height: "20px" }}
+                                    alt={`${matchup.awayTeamShortName} logo`}
+                                />
+                                <h6>{matchup.awayTeamShortName}</h6>
+                                <h6>
+                                    <strong>{matchup.aggregatedResult.awayScore}</strong>
+                                </h6>
+                            </div>
+                        </div>
+                    );
                 })}
+            </div>
+        );
+    })}
+</div>
+</>}
+                    
             </div>
     );
 };
