8dc205131dcfe30347a75454dbb7c6cfe7bdc8f1
Edward Yambasu <142278488+EdwardYambasu12@users.noreply.github.com>
2025-03-04 21:12:35 +0000

    Update ezoic.jsx

--- src/ezoic.jsx
+++ src/ezoic.jsx
@@ -1,15 +1,1 @@
-import { useEffect } from "react";
 
-const EzoicAd = () => {
-  useEffect(() => {
-    if (window.ezstandalone) {
-      window.ezstandalone.cmd.push(() => {
-        window.ezstandalone.showAds(101);
-      });
-    }
-  }, []);
-
-  return <div id="ezoic-pub-ad-placeholder-101"></div>;
-};
-
-export default EzoicAd;
