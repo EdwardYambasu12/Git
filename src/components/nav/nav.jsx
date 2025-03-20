6900987202ef127ea1049946a277b635536d480c
EdwardYambasu12 <yambasuedward4@gmial.com>
2025-03-20 15:09:53 +0000

    God innit

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1227,7 +1227,25 @@ async function fetcher(){
 			</div>
 
 			<div className = "live_score" style = {{height : window.innerHeight-100, overflowY : "auto", overflowX : "hidden"}}>{statement}
-
+    <footer className="footer">
+      <div className="circle left"></div> {/* Left Circle */}
+      <div className="footer-content">
+        <p>© Copyright 2025 LoneScore</p>
+        <div className="links">
+       
+          <a href="/privacy-policy">Privacy Policy</a>
+        </div>
+        <p>The use of automatic services (robots, crawlers, etc.) is not permitted.</p>
+        <div className="social-icons">
+          <a href="#"><i className="fab fa-tiktok"></i></a>
+          <a href="#"><i className="fab fa-instagram"></i></a>
+          <a href="#"><i className="fab fa-facebook"></i></a>
+          <a href="#"><i className="fab fa-linkedin"></i></a>
+          <a href="#"><i className="fab fa-x-twitter"></i></a>
+        </div>
+      </div>
+      <div className="circle right"></div> {/* Right Circle */}
+    </footer>
       </div>
 			
 			<div className = "ads_news">
